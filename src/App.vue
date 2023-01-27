0ae1cd6783f69ad2afc77097489c841633b98ac2
Abodunrin Abdulsalam <abdulsalamabodunrin369@gmail.com>
2023-01-27 13:05:01 +0100

    fix: set up tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,30 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
+  <main class="mainWrapper">
+    <header>
+      <div class="wrapper">
+        <nav class="flex mb-4 gap-5">
+          <RouterLink to="/" class="btn btn-green">Home</RouterLink>
+          <RouterLink to="/about" class="btn btn-green">About</RouterLink>
+        </nav>
+      </div>
+    </header>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+    <RouterView />
+  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style>
+.mainWrapper {
+  @apply max-w-screen-lg mx-auto my-5;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.btn {
+  @apply py-2 px-4 font-semibold rounded-lg shadow-md;
 }
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.btn-green {
+  @apply text-slate-500 hover:text-slate-700;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,20 @@
-<script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="boxWrapper">
+      <div class="box">To-Do App</div>
+      <div class="box">Habit Tracking App</div>
+    </div>
   </main>
 </template>
+
+<style scoped>
+.boxWrapper {
+  @apply flex gap-2 w-full;
+}
+.box {
+  @apply rounded-md bg-slate-800 p-2
+   shadow text-yellow-50 h-24 w-full max-w-sm flex-1;
+}
+</style>
